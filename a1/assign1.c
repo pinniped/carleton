5e19d28dfa3dcca5663b30743529fe597e861407
pinniped <abstract.wolf@gmail.com>
2012-09-20 14:24:44 -0400

    Update

--- a1/assign1.c
+++ a1/assign1.c
@@ -11,51 +11,12 @@
 #define NUM_PEGS  3
 #define NUM_COLOURS 8
 
-/* Initialize data structures used to maintain the colours.  
- * @param	A string containing one-letter names for all available colours
- * @return      Returns the number of colours available 
- */
 int initializeGame(char colours[]);
-
-/* Reset the clue to be "all-wrong" (where wrong is represented as a blank-character, or any character
- * not present in the colours-array.
- * @param  clue		The clue feedback given back to the user, stored as a STRING.
- */
 void clearClue(char clue[]);
-
-/* Prompt the user with the provided string and wait for a single-character response.
- * @param	prompt		A string to be printed as a prompt to the user for input.
- * @return	The first character typed by the user in response.    
- */
 char prompt_and_inputChar( char prompt[] );
-
-/* Prompt the user with the provided string and wait for a string response 
- *    (i.e. sequence of character with no whitespace).
- * @param	prompt		A string to be printed as a prompt to the user for input.
- * @param	input		A string typed in by the user in response to the prompt.    
- */
 void prompt_and_inputString( int n,  char prompt[], char input[] );
-
-/* Generate a sequence of random colours, using the provided set of legal colour-"names"
- * @param	colours	A set of one-letter names for legal colours
- * @param    nColours	The number of colours provided in colours
- * @param	solution	The randomly generated colours, stored as a STRING.
- */
 void generateSolution(char colours[], int nColours, char solution[]);
-
-/* Compares the secret solution with the user's guess, returning clues consisting of Black-Bulls and
- * White-Cows. (See Background information)
- * @param	solution		The correct, secret solution that the user is trying to guess
- * @param	guess		The current guess provided by the user
- * @param	clue			A list of clues, containing Black-blues (correct colour & position) and White-cows
- *						(correct colour but wrong position)
- * @return	True if the guess is completely correct; otherwise, false.
- */
 bool match(char solution[], char guess[], char clue[]);
-
-/* Prints the string at given location to console.
- * @param 	prompt 	Pointer to string to print.
- */
 void printStr(char *prompt);
 
 int main(void)
@@ -70,9 +31,9 @@ int main(void)
 	char guess[NUM_PEGS+1];		// The most recent guess entered by the user
 	char clue[NUM_PEGS+1];		// The clues returned to the user, made up of Black(Bulls) and White(Cows)
 
+	// TEST
 	//printf("Size of colours array is: %d.\n", test);
-	char c = prompt_and_inputChar("Enter a character please: ");
-	printf("Your char was: %c\n", c);
+	// clearClue("TEST");
 	return 0;
 }
 
@@ -95,7 +56,14 @@ int initializeGame(char colours[])
  */
 void clearClue(char clue[])
 { 
-
+	//TEST
+	//printf("%c\n", *clue);
+	while (*clue != '\0') {
+		*clue = '#';
+		clue++;
+	}
+	//TEST
+	//printf("%c\n", *(clue - 3));
 
 }
 
--- l1/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(int argc, const char *argv[])
-{
-	int i = 2<3;
-	int j = 2!=2;
-	int k = ++i;
-	printf("%d %d\n",j,k);
-	return 0;
-}
